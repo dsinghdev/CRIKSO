5e2e4a8a794e84578bfbe396b4e21b486dbc454e
d4man <d4man16@gmail.com>
2022-10-19 22:57:30 +0530

    Update About.php

---
+++ cricinfo/About.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Live Scores &amp; Updates </title>
+	<link   rel="stylesheet" href="css/style.css" type="text/css"> </link>
+</head>
+<body>
+<div>
+	<header>
+		<h1>Live Scores</h1>
+	</header>
+	<nav>
+	   <ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="liveScores.php" ""target="_blank">Live scores</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="admin/login.php">Admin Panel</a></li>
+
+	   </ul>
+	</nav>
+	<section>
+		<div>
+			<p>Crickso is an web app where statistics of sports are recorded for each player during a match, and aggregated over a career. It is basically used to maintain a collection of relevant, historical, statistics that can provide a competitive advantage to a team or individual. Through the collection and analyzation of these data, sports analytics inform players, coaches and other staff in order to facilitate decision making both during and prior to sporting events. 
+            </p>
+			<p>As technology has advanced over the last number of years data collection has become more in-depth and can be conducted with relative ease. Advancements in data collection have allowed for sports analytics to grow as well, leading to the development of advanced statistics throught Computer Science and Engineering.
+</p>
+</div>	
+	</section>
+	<footer>
+		
+	</footer>
+</body>
+</html>
